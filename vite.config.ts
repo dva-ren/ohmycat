a5d6f1a57757f9dd6e1935e33970a8c696df9f01
dva-ren <iiuzrw@gmail.com>
2022-12-25 08:33:51 +0800

    chore: clear dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,6 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
-import fs from 'fs-extra'
-import matter from 'gray-matter'
 
 export default defineConfig({
   resolve: {
@@ -23,16 +21,12 @@ export default defineConfig({
     }),
     // https://github.com/hannoeru/vite-plugin-pages
     Pages({
-      extensions: ['vue', 'md'],
-      dirs: ['src/pages/', 'src/posts', 'src/projects'],
+      extensions: ['vue'],
+      dirs: ['src/pages/'],
       extendRoute(route) {
         const path = resolve(__dirname, route.component.slice(1))
-
-        if (!path.includes('projects.md')) {
-          const md = fs.readFileSync(path, 'utf-8')
-          const { data } = matter(md)
-          route.meta = Object.assign(route.meta || {}, { frontmatter: data })
-        }
+        if (!path.includes('projects.md'))
+          route.meta = Object.assign(route.meta || {})
         return route
       },
     }),
